434e75d6f6271b63460af6849e1dc5fe81e07fb5
xasanovgo <akromjonxasanov23@gmail.com>
2023-07-28 17:55:31 +0500

    categories screen added

--- lib/ui/tab/tab_box.dart
+++ lib/ui/tab/tab_box.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:najot_shop/providers/tab_provider.dart';
 import 'package:provider/provider.dart';
 
 import '../../providers/auth_provider.dart';
@@ -13,13 +14,14 @@ class TabBox extends StatefulWidget {
 class _TabBoxState extends State<TabBox> {
   @override
   Widget build(BuildContext context) {
-    var provider = Provider.of<AuthProvider>(context,listen: true);
+    var provider = Provider.of<TabProvider>(context,listen: true);
     return Scaffold(
-      body: provider.screens[provider.activeIndex],
+      body: provider.screens[provider.activeIndex] ,
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: provider.activeIndex,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.production_quantity_limits_outlined), label: "Product"),
+          BottomNavigationBarItem(icon: Icon(Icons.category), label: "Categories"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
         ],
         onTap: (index){
